e9c82b35062da7dc1b8ad6c73934c2b214b622b4
Joni Kettunen <reisiluu@gmail.com>
2016-09-16 21:36:56 +0300

    Function with a parameter
    * Test with parameterized function

--- src/Dancer.clj
+++ src/Dancer.clj
@@ -1,6 +1,11 @@
  (ns Dancer)
 
+ (defn fun-times [name]
+   (println (str "I like your dancing " name "! Keep it going!")))
+
  (defn start []
-   (println "Look at my moves!"))
+   (do
+     (fun-times "Joni")
+     (println "Look at my moves!")))
 
  (start)
